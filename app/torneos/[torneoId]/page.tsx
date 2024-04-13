5404998a148d5489e76ff85f74fadb285dcc69d8
Tomas <jtomastahan@gmail.com>
2024-04-13 19:46:42 -0400

    Añadido de torneos prueba p.7

--- app/torneos/[torneoId]/page.tsx
+++ app/torneos/[torneoId]/page.tsx
@@ -6,17 +6,17 @@ export default async function TorneoId({ params }: any) {
 
   const supabase = createClient();
 
+  //   const { data, error } = await supabase
+  //     .from("vista_partidos_por_categoria")
+  //     .select("*")
+  //     .eq("americanoId", id)
+  //     .or("tipo.is.null,tipo.neq.Playoffs");
+
   const { data, error } = await supabase
     .from("vista_partidos_por_categoria")
     .select("*")
-    .eq("americanoId", id)
-    .or("tipo.is.null,tipo.neq.Playoffs");
-
-  // const { data, error } = await supabase
-  //   .from("vista_partidos_por_categoria")
-  //   .select("*")
-  //   .eq("americanoId", id) // Filtra por el ID del evento americano
-  //   .eq("tipo", "Playoffs");
+    .eq("americanoId", id) // Filtra por el ID del evento americano
+    .eq("tipo", "Playoffs");
 
   //   const data = [
   //     {
