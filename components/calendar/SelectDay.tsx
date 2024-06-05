d51bc35ffabbd1a8d707d140fbff0d3872a8c83e
Tomas <jtomastahan@gmail.com>
2024-06-05 12:36:42 -0400

    Cambios de calendar. Date-fns

--- components/calendar/SelectDay.tsx
+++ components/calendar/SelectDay.tsx
@@ -1,62 +1,62 @@
-"use client";
+// "use client";
 
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Button } from "../ui/button";
-import { addDays, subDays, format, parseISO, isValid } from "date-fns";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+// import { ChevronLeft, ChevronRight } from "lucide-react";
+// import { Button } from "../ui/button";
+// import { addDays, subDays, format, parseISO, isValid } from "date-fns";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { useEffect, useState } from "react";
 
-export default function SelectDay() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
+// export default function SelectDay() {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
 
-  const [fecha, setFecha] = useState(new Date());
+//   const [fecha, setFecha] = useState(new Date());
 
-  useEffect(() => {
-    const fechaParam = searchParams.get("fecha");
-    if (fechaParam) {
-      const nuevaFecha = parseISO(fechaParam);
-      if (isValid(nuevaFecha)) {
-        setFecha(nuevaFecha);
-      }
-    } else {
-      const hoy = format(new Date(), "yyyy-MM-dd");
-      router.replace(`/?fecha=${hoy}`);
-    }
-  }, [searchParams]);
+//   useEffect(() => {
+//     const fechaParam = searchParams.get("fecha");
+//     if (fechaParam) {
+//       const nuevaFecha = parseISO(fechaParam);
+//       if (isValid(nuevaFecha)) {
+//         setFecha(nuevaFecha);
+//       }
+//     } else {
+//       const hoy = format(new Date(), "yyyy-MM-dd");
+//       router.replace(`/?fecha=${hoy}`);
+//     }
+//   }, [searchParams]);
 
-  const avanzarDia = () => {
-    const nuevaFecha = addDays(fecha, 1);
-    setFecha(nuevaFecha);
-    router.replace(`/?fecha=${format(nuevaFecha, "yyyy-MM-dd")}`);
-  };
+//   const avanzarDia = () => {
+//     const nuevaFecha = addDays(fecha, 1);
+//     setFecha(nuevaFecha);
+//     router.replace(`/?fecha=${format(nuevaFecha, "yyyy-MM-dd")}`);
+//   };
 
-  const retrocederDia = () => {
-    const nuevaFecha = subDays(fecha, 1);
-    setFecha(nuevaFecha);
-    router.replace(`/?fecha=${format(nuevaFecha, "yyyy-MM-dd")}`);
-  };
+//   const retrocederDia = () => {
+//     const nuevaFecha = subDays(fecha, 1);
+//     setFecha(nuevaFecha);
+//     router.replace(`/?fecha=${format(nuevaFecha, "yyyy-MM-dd")}`);
+//   };
 
-  const setHoy = () => {
-    const nuevaFecha = new Date();
-    setFecha(nuevaFecha);
-    router.replace(`/?fecha=${format(nuevaFecha, "yyyy-MM-dd")}`);
-  };
+//   const setHoy = () => {
+//     const nuevaFecha = new Date();
+//     setFecha(nuevaFecha);
+//     router.replace(`/?fecha=${format(nuevaFecha, "yyyy-MM-dd")}`);
+//   };
 
-  return (
-    <div className="flex gap-2">
-      <Button variant="outline" onClick={retrocederDia} className="px-2">
-        <ChevronLeft size={20} />
-      </Button>
+//   return (
+//     <div className="flex gap-2">
+//       <Button variant="outline" onClick={retrocederDia} className="px-2">
+//         <ChevronLeft size={20} />
+//       </Button>
 
-      <Button variant="outline" onClick={setHoy}>
-        Hoy
-      </Button>
-      <Button variant="outline" onClick={avanzarDia} className="px-2">
-        <ChevronRight size={20} />
-      </Button>
+//       <Button variant="outline" onClick={setHoy}>
+//         Hoy
+//       </Button>
+//       <Button variant="outline" onClick={avanzarDia} className="px-2">
+//         <ChevronRight size={20} />
+//       </Button>
 
-      {/* <p>{format(fecha, "dd/MM/yyyy", { locale: es })}</p> */}
-    </div>
-  );
-}
+//       {/* <p>{format(fecha, "dd/MM/yyyy", { locale: es })}</p> */}
+//     </div>
+//   );
+// }
